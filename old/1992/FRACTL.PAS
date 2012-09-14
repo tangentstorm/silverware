9a1864c28f9164accc627a2ffd50d4eb7b71a408
Michal J. Wallace <michal.wallace@gmail.com>
2012-09-14 09:21:11 -0500

    import of files dated 1992

---
+++ old/1992/FRACTL.PAS
@@ -0,0 +1,15 @@
+Program Fractal;
+Uses Graph;
+
+(*
+   (c) 1992 Michal J. Wallace
+   This program is being desinged to implement a fractal Design
+   on the display. The picture will be composed entirely of squares,
+   which will be different sizes and colors depending on the square's
+   Generation..
+
+*)
+
+
+Begin
+End.
