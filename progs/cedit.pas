14d0cbdef2ef6270718e162cbf1d837f1ba5e73d
tangentstorm <michal.wallace@gmail.com>
2012-11-11 17:24:23 -0600

    cedit: merged the two classes, removed a bunch of duplicate / un-necessary code

--- progs/cedit.pas
+++ progs/cedit.pas
@@ -3,42 +3,39 @@ program cedit;
 uses xpc, ll, fs, stri, num, cw, crt; {,crtstuff,crt,filstuff,zokstuff; }
 
   type
+
     //  duplicated in vuestuff
     pstringobj = ^stringobj;
     stringobj  = class ( node )
       s	: string[ 180 ];
       constructor fromstring( st : string );
     end;
-    listviewer = class( list )
+
+    listeditor = class( list )
       lightbar : pnode;
+      nlstring : string[ 6 ];
       thisline, numlines  : longint;
       y1, y2		  : integer;
       //   work	  : screentype;
       topline, bottomline : pnode;
       constructor init; override;
+      function load( path : string ) : boolean;
       procedure show;
-      procedure arrowup; virtual;
-      procedure arrowdown; virtual;
+      procedure arrowup;
+      procedure arrowdown;
       procedure home;
-      procedure _end; virtual;
-      procedure pageup; virtual;
-      procedure pagedown; virtual;
+      procedure _end;
+      procedure pageup;
+      procedure pagedown;
       procedure run;
     end;
-    listeditor = class ( listviewer )
-      constructor init; override;
-      procedure arrowup; override;
-      procedure arrowdown; override;
-      procedure _end; override;
-    end;
 
-  var nlstring : string[ 6 ];
 
   constructor stringobj.fromstring( st : string );
   begin self.s := st;
   end;
-
-  constructor listviewer.init;
+
+  constructor listeditor.init;
   begin
     inherited;
     y1 := 2;
@@ -48,8 +45,27 @@ uses xpc, ll, fs, stri, num, cw, crt; {,crtstuff,crt,filstuff,zokstuff; }
     bottomline := nil;
     lightbar := nil;
   end;
-
-  procedure listviewer.show;
+
+  function listeditor.load( path : string ) : boolean;
+    var txt : text; line : string;
+  begin
+    result := fs.exists( path );
+    if result then begin
+      //  need to check for io errors in here
+      assign( txt, path );
+      reset( txt );
+      while not eof( txt ) do begin
+	readln( txt, line );
+	// writeln( s );
+	inc( self.numlines );
+	self.append( new( pstringobj, fromstring( line )));
+      end;
+      close( txt );
+      self.nlstring := flushrt( n2s( self.numlines ), 6, '.' );
+    end;
+  end;
+
+  procedure listeditor.show;
   begin
     if self.topline = nil then home;
     cwritexy( 1, 1,
@@ -60,41 +76,7 @@ uses xpc, ll, fs, stri, num, cw, crt; {,crtstuff,crt,filstuff,zokstuff; }
   end;
 
 
-
-  procedure listviewer.arrowup;
-  begin
-    if topline <> first then
-    begin
-      dec(thisline);
-      topline := topline^.prev;
-      bottomline := bottomline^.prev;
-      //  writeto := @work;
-      //  scrolldown1(1,80,y1,y2,nil);
-      typos := y1; tcolor := 7;
-      cwrite(pstringobj(topline)^.s);
-      cwriteln('|%');
-      show;
-    end;
-  end;
-
-  procedure listviewer.arrowdown;
-  begin
-    if bottomline^.next <> first then
-    begin
-      inc(thisline);
-      bottomline := bottomline^.next;
-      topline := topline^.next;
-      //  writeto := @work;
-      //  scrollup1(1,80,y1,y2,nil);
-      typos := y2; tcolor := 7;
-      cwrite(pstringobj(bottomline)^.s);
-      cwriteln('|%');
-      show;
-    end;
-  end;
-
-
-  procedure listviewer.home;
+  procedure listeditor.home;
     var c : byte;
   begin
     if self.first = nil then exit;
@@ -113,106 +95,6 @@ uses xpc, ll, fs, stri, num, cw, crt; {,crtstuff,crt,filstuff,zokstuff; }
     self.show;
   end;
 
-
-  procedure listviewer._end;
-    var c : byte;
-  begin
-    if first = nil then exit;
-    bottomline := first^.prev;
-    topline := bottomline;
-    //  writeto := @work;
-    //  fillbox(1,y1,80,y2,$0F20);
-    txpos := 1; typos := y2;
-    thisline := numlines;
-    for c := y1 to y2-1 do
-      if topline^.prev <> bottomline then
-      begin
-	cwriteln('|w'+pstringobj(topline)^.s);
-	dec(thisline);
-	dec(typos,2);
-	topline := topline^.prev;
-      end;
-    cwriteln('|w'+pstringobj(topline)^.s);
-    show;
-  end;
-
-
-  procedure listviewer.pageup;
-    var c : byte;
-  begin
-    for c := y1 to y2-1 do arrowup;
-  end;
-
-  procedure listviewer.pagedown;
-    var c : byte;
-  begin
-    for c := y1 to y2-1 do arrowdown;
-  end;
-
-
-  procedure listviewer.run;
-    var done : boolean = false;
-  begin
-    self.home;
-    repeat
-      if crt.keypressed then case crt.readkey of
-	#27, ^C	: done := true;
-	#0	: case crt.readkey of
-		#72 : arrowup; // when you press the UP arrow!
-		#80 : arrowdown; // when you press the DOWN arrow!
-		#71 : home;
-		#79 : _end;
-		#73 : pageup;
-		#81 : pagedown;
-	      end
-      end
-    until done;
-  end;
-
-  constructor listeditor.init;
-  begin
-    inherited;
-    y1 := 1;
-    y2 := 12;
-  end;
-
-
-  procedure listeditor.arrowup;
-  begin
-    if topline <> first then
-    begin
-      dec(thisline);
-      topline := topline^.prev;
-      bottomline := bottomline^.prev;
-      //  writeto := @work;
-      //  scrolldown1(1,80,y1,y2,nil);
-      //  scrolldown1(1,80,14,25,nil);
-      typos := y1; tcolor := 7;
-      cwrite(pstringobj(topline)^.s);
-      cwriteln('|%');
-      colorxy(1,14,7,padstr(pstringobj(topline)^.s,80,' '));
-      show;
-    end;
-  end;
-
-  procedure listeditor.arrowdown;
-  begin
-    if bottomline^.next <> first then
-    begin
-      inc(thisline);
-      bottomline := bottomline^.next;
-      topline := topline^.next;
-      //  writeto := @work;
-      //  scrollup1(1,80,y1,y2,nil);
-      //  scrollup1(1,80,14,25,nil);
-      typos := y2; tcolor := 7;
-      cwrite(pstringobj(bottomline)^.s);
-      cwriteln('|%');
-      colorxy(1,25,7,padstr(pstringobj(bottomline)^.s,80,' '));
-      show;
-    end;
-  end;
-
   procedure listeditor._end;
     var c : byte;
   begin
@@ -234,13 +116,82 @@ uses xpc, ll, fs, stri, num, cw, crt; {,crtstuff,crt,filstuff,zokstuff; }
     cwriteln('|w'+pstringobj(topline)^.s);
     show;
   end;
-
 
+
+  procedure listeditor.pageup;
+    var c : byte;
+  begin for c := y1 to y2-1 do arrowup;
+  end;
+
+  procedure listeditor.pagedown;
+    var c : byte;
+  begin for c := y1 to y2-1 do arrowdown;
+  end;
+
+
+  procedure listeditor.run;
+    var done : boolean = false;
+  begin
+    self.home;
+    repeat
+      if crt.keypressed then case crt.readkey of
+	#27, ^C	: done := true;
+	#0	: case crt.readkey of
+		#72 : arrowup; // when you press the UP arrow!
+		#80 : arrowdown; // when you press the DOWN arrow!
+		#71 : home;
+		#79 : _end;
+		#73 : pageup;
+		#81 : pagedown;
+	      end
+      end
+    until done;
+  end;
+
+ procedure listeditor.arrowup;
+  begin
+    if topline <> first then
+    begin
+      dec(thisline);
+      topline := topline^.prev;
+      bottomline := bottomline^.prev;
+      //  writeto := @work;
+      //  scrolldown1(1,80,y1,y2,nil);
+      //  scrolldown1(1,80,14,25,nil);
+      typos := y1; tcolor := 7;
+      cwrite(pstringobj(topline)^.s);
+      cwriteln('|%');
+      colorxy(1,14,7,padstr(pstringobj(topline)^.s,80,' '));
+      show;
+    end;
+  end;
+
+
+  procedure listeditor.arrowdown;
+  begin
+    if bottomline^.next <> first then
+    begin
+      inc(thisline);
+      bottomline := bottomline^.next;
+      topline := topline^.next;
+      //  writeto := @work;
+      //  scrollup1(1,80,y1,y2,nil);
+      //  scrollup1(1,80,14,25,nil);
+      typos := y2; tcolor := 7;
+      cwrite(pstringobj(bottomline)^.s);
+      cwriteln('|%');
+      colorxy(1,25,7,padstr(pstringobj(bottomline)^.s,80,' '));
+      show;
+    end;
+  end;
+
+
   procedure prints( n : pnode );
   begin
     cwriteln( pstringobj(n)^.s );
   end;
 
+
   //  not used
   FUNCTION Cipher (St, Passwd: String): String;
     VAR SPtr, PPtr: Integer;
@@ -257,39 +208,20 @@ uses xpc, ll, fs, stri, num, cw, crt; {,crtstuff,crt,filstuff,zokstuff; }
     Cipher := St;
   END;
 
-  var
-    ed	    : listeditor;
-    i	    : byte;
-    txt	    : text;
-    path, s : string;
-    done    : boolean;
-
+
+  var ed : listeditor;
 begin
-  crt.clrscr;
   randseed := 193;
-  //  doscursoroff;
-  //  setupcrt;
-  colorxy( 1, 13, 1, stri.ntimes( '─', 80 ));
-  colorxyc( 40, 6, 7, 'Loading...' );
-
   ed := listeditor.init;
-
-  path := paramstr( 1 );
-  if fs.exists( path ) then begin
-    assign( txt, path );
-    reset( txt );
-    while not eof( txt ) do begin
-      readln( txt, s );
-      // writeln( s );
-      inc( ed.numlines ); //  move to listviewer.append
-      ed.append( new( pstringobj, fromstring( s )));
-    end;
-    close( txt );
-  end;
-
-  nlstring := flushrt( n2s( ed.numlines ), 6, '.' ); //  move to listview
-
-  ed.run;
-  ed.destroy;
-  //  doscursoron;
+  if paramcount = 0 then
+    writeln( 'usage : cedit <filename>' )
+  else if ed.load( paramstr( 1 )) then
+  begin
+    //  doscursoroff;
+    //  setupcrt;
+    ed.run;
+    ed.destroy;
+    //  doscursoron;
+  end
+  else writeln( 'unable to load file: ', paramstr( 1 ));
 end.
