e9d231989962bae9d3c7e5c4048427d61732aa1c
tangentstorm <michal.wallace@gmail.com>
2013-01-28 12:29:50 -0600

    zmenu changes from [jan 17] + newly merged version of lib/xpl

--- work/zmenu.pas
+++ work/zmenu.pas
@@ -1,6 +1,6 @@
 {$i xpc }
 program zmenu;
-uses xpc, ui, fx, cw, num;
+uses xpc, ui, fx, cw, num, cx, kvm, cli;
 
 type
   Options = record
@@ -12,20 +12,21 @@ type
 Var
   zf         : Text;
   numEntries : Byte;
-  userChoice : Byte;            { the byte returned based on the user's choice }
-  choice     : ui.zChoice;      { just a variable for working with choices }
+  userChoice : Byte;       { the byte returned based on the user's choice }
+  choice     : ui.zChoice; { just a variable for working with choices }
   i          : Byte;
   data       : array [ 1 .. 20 ] of Options;
   menu       : ui.zMenu;
-
+const  bluebox  = $19B1;
 begin
 
-  // 
-  // fillscreen( bluebox );
+  //  filling the background just makes a mess.
+  // probably crt and video conflicting...
+  // fillscreen( $3333, '#'); //'▒' );
   // DosCursorOff;
+  cwriteln('|$|!k|w');
 
-  txPos      := 1;
-  tyPos      := 1;
+
   numEntries := 0;
 
   assign( zf, 'zmenu.dat' );
@@ -38,33 +39,34 @@ begin
   end;
   close( zf );
 
-
   fx.button( 13, 4, 33, numEntries + 7 );
-  fx.greyshadow( 13, 4, 33, numEntries + 7 );
 
-  menu := ui.newMenu( on, on,
-    ui.newChoiceXY( 15, 5,
-              '|!w |b═|B═|C═|b[|Y·|WZMenu|Y·|b]|C═|B═|b═', '',
-              false, ' ', 0, nil, nil ));
+  //  fx.greyshadow( 13, 4, 33, numEntries + 7 );
+  choice := ui.newChoiceXY( 15, 5,
+			   '|!w |b═|B═|C═|b[|Y·|WZMenu|Y·|b]|C═|B═|b═', '',
+			   false, ' ', 0, nil );
 
-  for i := 1 to numentries
-  do menu.add(
-       ui.newChoiceXY( 14, i + 5,
-         cpadstr('|!w |r(|W' + N2S( i ) + '|r) |K'
-                 + data[ i ].menutext, 20, ' ' ),
-         cpadstr('|!k |R(|W' + N2S( i ) + '|R) |W'
-                 + data[ i ].menutext, 20, ' ' ),
-         true, Char( 48 + i ), i, nil, nil ));
+  menu := ui.newMenu( on, on, choice );
+
+  for i := 1 to numentries do
+    menu.add(
+      ui.newChoiceXY( 14, i + 5,
+		     cpadstr('|!w |r(|W' + N2S( i ) + '|r) |K'
+			     + data[ i ].menutext, 20, ' ' ),
+		     cpadstr('|!k |R(|W' + N2S( i ) + '|R) |W'
+			     + data[ i ].menutext, 20, ' ' ),
+		     true, Char( 48 + i ), i, nil ));
+
+
   menu.add(
     ui.newChoiceXY( 14, i + 6,
-      cpadstr('|!w |r(|W0|r)|K Quit', 20, ' '),
-      cpadstr('|!k |R(|W0|R)|W Quit', 20, ' '),
-      true, '0', 0, nil, nil ));
+		   cpadstr('|!w |r(|W0|r)|K Quit', 20, ' '),
+		   cpadstr('|!k |R(|W0|R)|W Quit', 20, ' '),
+		   true, '0', 0, nil ));
 
   //  mouseOn;
   userChoice := menu.get;
   //  dosCursorOn;
-
   if userChoice = 0 then halt( 0 );
 
 end.
